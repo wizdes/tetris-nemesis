42ed5f1d71a98f92fa0b1f901e96cb28d8e9ebfd
Yi Li <yl2326@cornell.edu>
2015-02-14 16:51:29 -0800

    added 3rd canvas

--- TetrisDemo/TetrisDemo/TetrisJs/Menu.js
+++ TetrisDemo/TetrisDemo/TetrisJs/Menu.js
@@ -3,6 +3,15 @@
     stage.enableMouseOver();
     var startButton = createButton("START");
 
+    var canvas = document.getElementById('menuCanvas');
+    var context = canvas.getContext('2d');
+
+    // draw blue rectangle
+    context.beginPath();
+    context.rect(200, 20, 100, 100);
+    context.fillStyle = 'blue';
+    context.fill();
+
     stage.addChild(startButton);
     stage.update();
 }
