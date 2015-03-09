5d8b9eb40f2b9af1fa431fb799d9c05e7e94ccdb
Yi Li <yl2326@cornell.edu>
2015-03-08 22:22:10 -0700

    added the controller class

---
+++ TetrisDemo/TetrisDemo/TetrisMVC/Controller/Controller.js
@@ -0,0 +1,20 @@
+﻿// root class
+// singleton
+
+function controller(typeName) {
+    this.typeName = typeName;
+
+    this.getTypeInfo = function() {
+        return "basicController";
+    }
+
+    this.run = function() {
+        alert("error");
+    }
+}
+
+keyController.prototype = new controller();
+keyController.prototype.constructor = keyController;
+function keyController(typeName) {
+    this.typeName = typeName;
+}
